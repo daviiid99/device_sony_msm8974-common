79f07e870a890c30621ee2d29f72f7e858519047
David <davidmolinamorales1999@gmail.com>
2021-05-11 11:25:17 +0200

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -140,7 +140,7 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_USES_INTERACTION_BOOST := true
 
 # SELinux
-include device/qcom/sepolicy-legacy/SEPolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/sony/msm8974-common/sepolicy
